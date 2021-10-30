939c60c9433948290b96665870899341e25679a1
Jashandeep singh <62203228+jashandeep1659@users.noreply.github.com>
2021-10-30 17:49:50 +0530

    Update context_processors.py

--- core/context_processors.py
+++ core/context_processors.py
@@ -20,7 +20,8 @@ def blogs(request):
         recent_blogs_pages = recent_blogs_page.page(recent_blogs_page.num_pages)
 
     tags =  random.sample(list(Tag.objects.all()), 4)
-    category = random.sample(list(Category.objects.filter(id__in = trending_blogs.values('category_id'))),1 )
+#     category = random.sample(list(Category.objects.filter(id__in = trending_blogs.values('category_id'))),1 )
+    category = Category.objects.all()
     about_me  = About_me.objects.get(id =1)
     context = {
     'trending_blogs':trending_blogs,
